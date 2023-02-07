54476d939b29f009dd03d2eee99ea1a8ffe5a753
1323216010 <101637168+1323216010@users.noreply.github.com>
2023-02-07 11:44:51 +0800

    1 file modified

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ const canvas = document.getElementById('deepar-canvas');
 const deepAR = new DeepAR({
   licenseKey: 'e7354dce189b6f111ce2d13d93e484a78b25001e569db34d5b3cfc185cebddc0e0edc228d207230b',
   canvas: canvas,
-  deeparWasmPath: '/config/deepar.wasm',
+  deeparWasmPath: 'config/deepar.wasm',
   callbacks: {
     onInitialize: () => {
       deepAR.startVideo(true);
     },
   },
   segmentationConfig: {
-    modelPath: '/config/models-68-extreme.bin',
+    modelPath: 'config/models-68-extreme.bin',
   },
 });
 
-deepAR.downloadFaceTrackingModel('/config/segmentation-160x160-opt.bin');
+deepAR.downloadFaceTrackingModel('config/segmentation-160x160-opt.bin');
 
 const effects = [
   './effects/test.deepar',
